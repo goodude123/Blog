2cb0193c3e9bcfd6dd88ead7c36f588dea441e6e
goodude123 <assasinmcpl@gmail.com>
2018-11-17 02:00:57 +0100

    New form to adding articles.

--- cms/urls.py
+++ cms/urls.py
@@ -6,5 +6,6 @@ app_name = 'cms'
 urlpatterns = [
     path('', views.Main.as_view(), name='main'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/register/', views.register, name='register')
+    path('accounts/register/', views.register, name='register'),
+    path('add_article/', views.add_article, name='add_article')
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
+from django.utils import timezone
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -8,10 +9,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from .models import Article
 from .decorators import prevent_logged
+from .forms import AddArticleForm
 
 
 class Main(ListView):
-    template_name = 'articles_list.html'
+    template_name = 'cms/articles_list.html'
     context_object_name = 'articles'
     model = Article
     
@@ -42,7 +44,27 @@ def register(request):
 
 @login_required
 def add_article(request):
-    return HttpResponse
+    if request.method == 'POST':
+        form = AddArticleForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            author = request.user
+            pub_date = timezone.now()
+            article = Article(
+                title=title,
+                content=content,
+                author=author,
+                pub_date=pub_date
+            )
+            article.save()
+            return redirect(reverse('cms:main'))
+        else:
+            redirect(reverse('cms:add_article'))
+
+    elif request.method == 'GET':
+        form = AddArticleForm()
+        return render(request, 'cms/add_article.html', {'form': form})
 
 
 @login_required
