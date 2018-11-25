439c3f358fea50c0e877f915f44d8a417902593d
goodude123 <assasinmcpl@gmail.com>
2018-11-25 20:31:58 +0100

    Api paths included.

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cms.urls'))
+    path('', include('cms.urls')),
+    path('api/', include('api.urls')),
 ]
