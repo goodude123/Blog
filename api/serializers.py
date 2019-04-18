fec8db54f0a6e3238b58dec3ad9c824b5ce82b50
goodude123 <assasinmcpl@gmail.com>
2019-04-18 16:07:25 +0200

    Fixed.

--- api/serializers.py
+++ api/serializers.py
@@ -20,7 +20,7 @@ class UserSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
-            first_name=validated_data['last_name'],
+            last_name=validated_data['last_name'],
         )
         
         user.set_password(validated_data['password'])
