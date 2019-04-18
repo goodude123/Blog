354be6c4bcf6c49cc1a2ca69699ac648ebca375d
goodude123 <assasinmcpl@gmail.com>
2019-04-18 15:33:16 +0200

    User model serializer.

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from cms.models import Article
 
 
@@ -6,3 +7,32 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('id', 'title', 'content', 'author', 'pub_date')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name')
+        write_only_fields = ('password',)
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        
+        try:
+            user.first_name = validated_data['first_name']
+        except KeyError:
+            pass
+
+        try:
+            user.first_name = validated_data['last_name']
+        except KeyError:
+            pass
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
